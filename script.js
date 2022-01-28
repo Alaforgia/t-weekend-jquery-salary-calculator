bbf1c21eb7d38163e15aca7656233027fad70061
Tony LaForgia <alforgia428@gmail.com>
2022-01-28 16:37:34 -0600

    added css + functions

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(readyNow);
 
-function readyNow(){
-    console.log("is working");
+function readyNow() {
+  console.log("is working");
+  clickSubmit();
+  $("#submit-button").on("click", clickSubmit);
+}
+
+function clickSubmit() {
+  //   let firstName = $("#input-first-name").submit("");
+  console.log("works");
+  let addFirstName = $("#input-first-name").val();
+  $("#employ-first-name").append(addFirstName);
 }
